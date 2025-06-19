05ae4294105ef172781c4a6a1f00a8f06918df4f
Myriam-Mabrouki <116686541+Myriam-Mabrouki@users.noreply.github.com>
2025-06-19 15:40:01 +0200

    Update comments and rename n_executions.sh to energy_measurements.sh

--- Scripts/energy_measurements.sh
+++ Scripts/energy_measurements.sh
@@ -1,13 +1,15 @@
 #! /bin/bash
 
 # This script executes n times a chosen program
+# This script is used in our experiments for executing a program 
+# while measuring the whole energy consumpion.
 
 n=$1		#1st argument = number of executions
 prgm=$2		#2nd argument = name of the chosen program
 
 # The chosen program has its priority set to the maximum possible
-# for a program in the user space
-for  ((i=0; i < $n; i++))
+# (in the user space)
+for  ((i=0; i<$n; i++))
 do
 	sudo nice -n -20 ./$prgm
 done
