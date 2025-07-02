c0b6498ea6dadce290d2df8ca1a3037883d6caa7
Myriam Mabrouki <mmabrouk@sta-02008654.paris.inria.fr>
2025-07-02 12:22:18 +0200

    comments update in make_executables

--- make_executables.sh
+++ make_executables.sh
@@ -3,22 +3,37 @@
 # This script create a directory, compile TACLeBench programs 
 # and move them into the created directory.
 
+# 1 - Get TACLeBench tasks
+# Download TACLeBench repository from github
 wget https://github.com/tacle/tacle-bench/archive/master.tar.gz
+# Exact the .tar.gz file
 tar -xvzf master.tar.gz
+# Rename the repository
 mv master TACLeBench
+# Delete the .tar.gz file
 rm master.tar.gz
+
+# 2 - Create a directory 
+# Delete "executables" directory if it exists
 rmdir -rf executables
+# Create a new one
 mkdir executables
+# Go to the location of the programs 
 cd TACLeBench/tacle-bench-master/bench/sequential/
 
-# For each program compile it and move the executables
-# into the previously created directory
+# 3 - Compile and move each task into "executables" directory
+# For each task...
 for dir in *
 do	
+	# Display its directory
 	echo $dir
+	# Go to its directory
 	cd $dir
+	# Compile it
 	gcc *.c
+	# Rename the executable
 	mv a.out $dir
+	# Move the executables into the dedicated directory
 	mv $dir ../../../../../executables
 	cd ..
 done
