6abd6d692bdecace3dd4ee79d3f30c6f98d7e271
Myriam Mabrouki <mmabrouk@sta-02008654.paris.inria.fr>
2025-07-02 12:19:42 +0200

    comments update in time_measurements

--- time_measurements.sh
+++ time_measurements.sh
@@ -3,9 +3,13 @@
 # This script executes n times TACLeBench programs and retrieves the following information : 
 # total CPU time, number of cycles, number of instructions, total elapsed time.
 
+# 1 - Check the parameters
 if [ "$#" -ne 3 ]; then
+    # Display the error message in stderr
     echo "Illegal number of parameters" >&2
+    # Display the expected format in stderr
     echo "Expected : ./time_measurements.sh number_of_executions CPU_frequency memory_frequency" >&2
+    # End the program with an error 
     exit 2
 fi
 
@@ -14,17 +18,21 @@ n=$1		#1st argument = number of executions
 fCPU=$2		#2nd argument = current CPU frequency
 fMEM=$3		#3rd argument = current memory frequency
 
-# Delete "executables" directory if it exists and create a new one
+# 2 - Create a directory 
+# Delete "executables" directory if it exists
 rm -rf ./time_measures_${fCPU}_${fMEM}
+# Create a new one
 mkdir time_measures_${fCPU}_${fMEM}
 
-# For each program in the benchmark
+# 3 - Execution time measures
+# For each task...
 for prgm in executables/*
 do
+	# For each execution
 	for  ((i=0; i < $n; i++))
 	do
-		# The program has its priority set to the maximum possible
-		# (in the user space)
+		# Program executed in isolation in core 3 with its priority set to the maximum
+		# Total CPU time, number of cycles, number of instructions and total elapsed time retrieved
 		sudo perf stat taskset -c 3 nice -n -20 $prgm  2>&1 | grep -e task-clock  -e cycles -e instructions -e elapsed  | awk {'print $1'} >> "time_measures_${fCPU}_${fMEM}/${prgm/*\/}_time_measure_fCPU_${fCPU}_fMEM_${fMEM}.txt"
 	done
 done
