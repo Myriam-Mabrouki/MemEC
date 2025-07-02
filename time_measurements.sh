22bd1d4a6fea497a46bec3a7cb1fb2d2fb208e49
Myriam-Mabrouki <myriam.mabrouki@tutanota.com>
2025-07-02 14:36:37 +0200

    removed blank spaces in time_measurements

--- time_measurements.sh
+++ time_measurements.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-# This script executes n times TACLeBench programs and retrieves the following information : 
+# This script executes n times TACLeBench programs and retrieves the following information :
 # total CPU time, number of cycles, number of instructions, total elapsed time.
 
 # 1 - Check the parameters
@@ -9,7 +9,7 @@ if [ "$#" -ne 3 ]; then
     echo "Illegal number of parameters" >&2
     # Display the expected format in stderr
     echo "Expected : ./time_measurements.sh number_of_executions CPU_frequency memory_frequency" >&2
-    # End the program with an error 
+    # End the program with an error
     exit 2
 fi
 
@@ -18,7 +18,7 @@ n=$1		#1st argument = number of executions
 fCPU=$2		#2nd argument = current CPU frequency
 fMEM=$3		#3rd argument = current memory frequency
 
-# 2 - Create a directory 
+# 2 - Create a directory
 # Delete "executables" directory if it exists
 rm -rf ./time_measures_${fCPU}_${fMEM}
 # Create a new one
