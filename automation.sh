e02bd296195d875196ae2e3d7ac1e59b4fb6765a
Myriam Mabrouki <mmabrouk@sta-02008654.paris.inria.fr>
2025-07-04 11:38:28 +0200

    add ask parameters during runtime

--- automation.sh
+++ automation.sh
@@ -11,15 +11,45 @@ if [ "$#" -ne 7 ]; then
     exit 2
 fi
 
-# Parameters
-N=$1		#1st argument = number of executions
-PRGM=$2		#2nd argument = name of the chosen program
-MIN=$3      #3rd argument = minimum CPU frequency tested (in MHz)
-MAX=$4      #4th argument = maximum CPU frequency tested (in MHz)
-STEP=$5     #5th argument = step
-fMEM=$6     #5th argument = memory frequency in MHz
-NRG=$7      #7th argument = 1 if we want to do energy consumption measures, 0 otherwise
-
+# Set the parameters
+if [ "$#" -eq 0 ]
+# During runtime
+then
+    # Ask user about the type of measurements
+    echo "Enter 1 if you would like to carry on energy consumption measurements, 0 for time measurements:"
+    # Read user's response
+    read ENERGY_OR_TIME
+    # Ask user about the number of executions
+    echo "How many executions would you like to do?"
+    read N
+    # Ask user about frequencies
+    echo "What is the minimum CPU frequency (in MHz) you want to use to carry on your experiments?"
+    read MIN
+    echo "What is the maximum CPU frequency (in MHz)?"
+    read MAX
+    echo "What step would you like to use between these two frequencies?"
+    read STEP
+    if (($ENERGY_OR_TIME != 1))
+    then
+        # Ask user about the program to measure for energy consumption
+        echo "What program would you like to measure ?"
+        read PRGM
+    else
+        # Ask user about memory frequency (which has to be already set)
+        echo "What is the memory frequency you are currently using for your time measurements ?"
+        read fMEM
+    fi
+# Before runtime
+else
+    # Parameters
+    N=$1		#1st argument = number of executions
+    PRGM=$2		#2nd argument = name of the chosen program
+    MIN=$3      #3rd argument = minimum CPU frequency tested (in MHz)
+    MAX=$4      #4th argument = maximum CPU frequency tested (in MHz)
+    STEP=$5     #5th argument = step
+    fMEM=$6     #5th argument = memory frequency in MHz
+    NRG=$7      #7th argument = 1 if we want to do energy consumption measures, 0 otherwise
+fi
 
 # 2 - Initial configurations
 # Set the CPUFreq governor
