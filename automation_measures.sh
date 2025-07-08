4042d14368172354c21257f9674f6d998248358f
Myriam Mabrouki <mmabrouk@sta-02008654.paris.inria.fr>
2025-07-08 11:32:17 +0200

    rename boolean variable

--- automation_measures.sh
+++ automation_measures.sh
@@ -18,7 +18,7 @@ MAX=1400                    # Maximum CPU frequency tested (in MHz)
 STEP=400                    # Step between two CPU frequencies tested
 fMEM=500                    # Memory frequency in MHz
 PRGM=executables/statemate  # Name of the chosen program
-ENERGY_OR_TIME=1            # 0 for time measurements, 1 for energy measurements
+ENERGY=1            # 0 for time measurements, 1 for energy measurements
 
 # 3 - Initial configurations
 # Set the CPUFreq governor
@@ -28,7 +28,7 @@ sudo cpupower frequency-set --min $(($MIN*1000)) --max $(($MAX*1000))
 
 # 4 - Measurements
 # Choose between doing time or energy consumption measures
-if [ $ENERGY_OR_TIME -ne 0 ]
+if [ $ENERGY -ne 0 ]
 then
     # Energy consumption measures
     for ((fCPU=$MIN; fCPU<=$MAX; fCPU=fCPU+$STEP))
