cb6a9f81c63a57c2aaa7bfbc33a08e071806dd48
Myriam Mabrouki <mmabrouk@sta-02008654.paris.inria.fr>
2025-07-09 11:55:03 +0200

    browse directory + able to get fCPU and fMEM

--- parcours.c
+++ parcours.c
@@ -9,31 +9,63 @@
 
 int main()
 {
-    DIR *d;
-    struct dirent *dir;
-    struct stat filestat;
+    DIR *d1, *d2;
+    struct dirent *dir1, *dir2;
+    struct stat filestat1, filestat2;
+    int fCPU, fMEM;
 
-    d = opendir("results/time_measures");
-    if (d) {
-        while ((dir = readdir(d)) != NULL) {
-            stat(dir->d_name,&filestat);
-            if( S_ISDIR(filestat.st_mode) && strcmp(dir->d_name, ".") && strcmp(dir->d_name, "..") ) {
-                int fCPU, fMEM, MIN_CPU = 600, MAX_CPU = 1400, STEP_CPU = 400, MIN_MEM = 200, MAX_MEM = 500, STEP_MEM = 100;
-            	for (fCPU = MIN_CPU; fCPU <= MAX_CPU; fCPU=fCPU+STEP_CPU) {
-            	    for (fMEM = MIN_MEM; fMEM <= MAX_MEM; fMEM=fMEM+STEP_MEM) {
-                    	char filename[MAX_LENGTH]="results/time_measures/";
-                    	char addition[MAX_LENGTH];
-            		    sprintf(addition, "%s/%s_time_measure_fCPU_%d_fMEM_%d", dir->d_name, dir->d_name, fCPU, fMEM);
-            	        strcat(filename, addition);
-            	        printf("%s\n", filename);
-            	    }
-            	}
+    d1 = opendir("results/time_measures");
+    if (d1) {
+        while ((dir1 = readdir(d1)) != NULL) {
+            char dirname[MAX_LENGTH];
+            sprintf(dirname, "results/time_measures/%s", dir1->d_name);
+            stat(dirname, &filestat1);
+            if( S_ISDIR(filestat1.st_mode) && strcmp(dir1->d_name, ".") && strcmp(dir1->d_name, "..") ) {
+                printf("%s\n", dirname);
+                char output_filename[MAX_LENGTH];
+                sprintf(output_filename, "results/time_measures/%s/%s_results.txt", dir1->d_name, dir1->d_name);
+                FILE* output_file = fopen(output_filename, "w");
+                d2 = opendir(dirname);
+                if (d2) {
+                    while ((dir2 = readdir(d2)) != NULL) {
+                        char filename[MAX_LENGTH];
+                        sprintf(filename, "results/time_measures/%s/%s", dir1->d_name, dir2->d_name);
+                        stat(filename, &filestat2);
+                        if( !S_ISDIR(filestat2.st_mode) && strcmp(filename, output_filename)) {
+                            printf("%s\n", filename);
+                            FILE* input_file = fopen(filename, "r");
+                            if (input_file) {
+                                char tmp[MAX_LENGTH];
+                                strcpy(tmp, dir2->d_name);
+                                strtok(tmp, "_");
+                                strtok(NULL, "_");
+                                strtok(NULL, "_");
+                                strtok(NULL, "_");
+                                fCPU = atoi(strtok(NULL, "_"));
+                                strtok(NULL, "_");
+                                fMEM = atoi(strtok(NULL, "_"));
+                                printf("fCPU %d, fMEM %d\n", fCPU, fMEM);
+                                fclose(input_file);
+                            } else {
+                                // Print an error message to the standard error stream if the file cannot be opened.
+                                fprintf(stderr, "Unable to open file!\n");
+                                // End the program with an error
+                                return EXIT_FAILURE;
+                            }
+                        }
+                    }
+                    closedir(d2);
+                    fclose(output_file);
+                } else {
+                    fprintf(stderr, "Unable to open directory %s\n", dirname);
+                    return EXIT_FAILURE;
+                }
             }
             
         }
-        closedir(d);
+        closedir(d1);
     } else {
-        fprintf(stderr, "Unable to open directory");
+        fprintf(stderr, "Unable to open directory results/time_measures");
         return EXIT_FAILURE;
     }
 
