edfbb1b92a56495013b9acfe0b50a11b03df362e
Myriam-Mabrouki <myriam.mabrouki@tutanota.com>
2025-07-15 12:04:34 +0200

    writing exec time while measuring energy

--- automation_measures.sh
+++ automation_measures.sh
@@ -5,11 +5,11 @@
 # 1 - Check the parameters
 if [ "$#" -ne 0 ]
 then
-    # Display the error message in stderr
-    echo "Illegal number of parameters" >&2
-    # Display the expected format in stderr
-    echo "Expected format is : ./automation_measures" >&2
-    exit 2
+	    # Display the error message in stderr
+	    echo "Illegal number of parameters" >&2
+	    # Display the expected format in stderr
+	    echo "Expected format is : ./automation_measures" >&2
+	    exit 2
 fi
 
 # 2 - Set the parameters
@@ -24,37 +24,43 @@ ENERGY=1                    # 0 for time measurements, 1 for energy measurements
 
 # 3 - Initial configurations
 # Set the CPUFreq governor
-sudo cpupower frequency-set --governor userspace
+cpupower frequency-set --governor userspace
 # Set minimum and maximum frequencies
-sudo cpupower frequency-set --min $(($MIN*1000)) --max $(($MAX*1000))
+cpupower frequency-set --min $(($MIN*1000)) --max $(($MAX*1000))
 
 # 4 - Measurements
 # Choose between doing time or energy consumption measures
 if [ $ENERGY -ne 0 ]
 then
-    # Energy consumption measures
-    for ((fCPU=$MIN; fCPU<=$MAX; fCPU=fCPU+$STEP))
-    do
-        # Set the frequency
-        sudo cpupower frequency-set --freq $(($fCPU*1000))
-        # Space the measures
-        sleep 1
-        # Do the measures
-        ./energy_measurements.sh $N $PRGM
-        # Space the measures
-        sleep 1
-    done
+	# Check if the file containing execution time exists
+	if ! [ -f results/time_measures/${PRGM/*\/}/${PRGM/*\/}_results.txt ]; then
+  		echo "exec_time,CPU_freq,MEM_freq" >> "results/time_measures/${PRGM/*\/}/${PRGM/*\/}_results.txt"
+	fi
+    	# Energy consumption measures
+    	for ((fCPU=$MIN; fCPU<=$MAX; fCPU=fCPU+$STEP))
+    	do
+        	# Set the frequency
+	        cpupower frequency-set --freq $(($fCPU*1000))
+        	# Space the measures
+	        sleep 1
+        	# Do the measures
+	        DURATION=`perf stat ./energy_measurements.sh $N $PRGM 2>&1 | grep elapsed  | awk '{print $1}'`
+        	# Space the measures
+	        sleep 1
+		# Write the duration
+		echo $DURATION,$fCPU,$fMEM >> "results/time_measures/${PRGM/*\/}/${PRGM/*\/}_results.txt"
+    	done
 else
-    # Time measures
-    for ((fCPU=$MIN; fCPU<=$MAX; fCPU=fCPU+$STEP))
-    do
-        # Set the frequency
-        sudo cpupower frequency-set --freq $(($fCPU*1000))
-        # Space the measures
-        sleep 1
-        # Do the measures
-        ./time_measurements.sh $N $fCPU $fMEM
-        # Space the measures
-        sleep 1
-    done
+	    # Time measures
+	    for ((fCPU=$MIN; fCPU<=$MAX; fCPU=fCPU+$STEP))
+	    do
+	        # Set the frequency
+	        cpupower frequency-set --freq $(($fCPU*1000))
+        	# Space the measures
+	        sleep 1
+	        # Do the measures
+        	./time_measurements.sh $N $fCPU $fMEM
+	        # Space the measures
+        	sleep 1
+	    done
 fi
