51af0f4f3c60f513ff48d61e981b4a457b1a96b3
Myriam Mabrouki <mmabrouk@sta-02008654.paris.inria.fr>
2025-07-15 15:48:26 +0200

    csv_modif begins 0

--- csv_modifier.c
+++ csv_modifier.c
@@ -5,25 +5,16 @@
 #include <sys/stat.h>
 #define MAX_LENGTH 1024
 
-int add_FCPU_fMEM_in_csv(FILE *input_file, FILE *output_file, float avg1, float avg2, float avg3, int fMEM) 
+int begin_at_0(FILE *input_file, FILE *output_file)
 {
-  // CPU frequency
-	int fCPU;
-	// Number of executions
-	int N = 1000;
-	// Intervals
-	float begin1 = -1, end1, begin2, end2, begin3, end3;
-
 	// Line counter
 	int lc = 0;
 	// Buffer to store each line of the input file.
 	char line[MAX_LENGTH];
-	// Buffer to store the addition going to the output file.
-	char addition[MAX_LENGTH];
+	// Buffer to store the new line going to the output file.
+	char newline[MAX_LENGTH];
 	// Timestamp for power consumption
-	float timestamp;
-	// Value of power consumption
-	float value;
+	float timestamp, first_timestamp, value;
 
 	// Read each line from the input file and store it in the 'line' buffer.
 	while (fgets(line, sizeof(line), input_file)) {
@@ -32,29 +23,73 @@ int add_FCPU_fMEM_in_csv(FILE *input_file, FILE *output_file, float avg1, float 
 		char tmp[MAX_LENGTH];
         strcpy(tmp, line);
         timestamp = atof(strtok(tmp, ","));
+		value = atof(strtok(NULL, ","));
 
-		if (begin1 == -1 && lc != 1) {
-			value = atof(strtok(NULL, ","));
-			if (value >= 2.6) {
-				begin1 = timestamp;
-				end1 = begin1 + N * avg1;
-				begin2 = end1 + 2;
-				end2 = begin2 + N * avg2;
-				begin3 = end2 + 2;
-				end3 = begin3 + N * avg3;
-			} else {
-				timestamp = -2;
-			}
+		if (lc == 1) {
+			fputs(line, output_file);
+			continue;
 		}
+		else if (lc == 2) {
+			first_timestamp = timestamp;
+		}
+
+		// CPU and memory frequencies
+		sprintf(newline, "%f,%f\n", timestamp - first_timestamp, value);
+		// Write the modified line into the output file.
+		fputs(newline, output_file);
+	}
+
+	return 0;
+}
+
+int add_FCPU_fMEM_in_csv(FILE *input_file, FILE *output_file, int fCPU, int fMEM) 
+{
+	// Number of executions
+	//int N = 1000;
+	// Intervals
+	//float begin1 = -1, end1, begin2, end2, begin3, end3;
+
+	// Line counter
+	int lc = 0;
+	// Buffer to store each line of the input file.
+	char line[MAX_LENGTH];
+	// Buffer to store the addition going to the output file.
+	char addition[MAX_LENGTH];
+	// Timestamp for power consumption
+	// float timestamp;
+	// Value of power consumption
+	// float value;
+
+	// Read each line from the input file and store it in the 'line' buffer.
+	while (fgets(line, sizeof(line), input_file)) {
+		// Increments counter
+		lc++;
+		//char tmp[MAX_LENGTH];
+        //strcpy(tmp, line);
+        // timestamp = atof(strtok(tmp, ","));
+
+		// if (begin1 == -1 && lc != 1) {
+		// 	value = atof(strtok(NULL, ","));
+		// 	if (value >= 2.6) {
+		// 		begin1 = timestamp;
+		// 		end1 = begin1 + N * avg1;
+		// 		begin2 = end1 + 2;
+		// 		end2 = begin2 + N * avg2;
+		// 		begin3 = end2 + 2;
+		// 		end3 = begin3 + N * avg3;
+		// 	} else {
+		// 		timestamp = -2;
+		// 	}
+		// }
 		
-		if (begin1 <= timestamp && timestamp <= end1)
-			fCPU = 600;
-		else if (begin2 <= timestamp && timestamp <= end2)
-			fCPU = 1000;
-		else if (begin3 <= timestamp && timestamp <= end3)
-			fCPU = 1400;
-		else
-			fCPU = -1;
+		// if (begin1 <= timestamp && timestamp <= end1)
+		// 	fCPU = 600;
+		// else if (begin2 <= timestamp && timestamp <= end2)
+		// 	fCPU = 1000;
+		// else if (begin3 <= timestamp && timestamp <= end3)
+		// 	fCPU = 1400;
+		// else
+		// 	fCPU = -1;
 		
 		// Remove the newline character.
 		line[strcspn(line, "\n")] = 0;
@@ -79,8 +114,8 @@ int main()
 	DIR *d1, *d2;
     struct dirent *dir1, *dir2;
     struct stat filestat1, filestat2;
-	float avg1, avg2, avg3;
-	float fMEM;
+	//float avg1, avg2, avg3;
+	int fCPU, fMEM;
 
 	d1 = opendir("results/energy_measures");
     if (!d1) {
@@ -92,7 +127,7 @@ int main()
 		sprintf(dirname, "results/energy_measures/%s", dir1->d_name);
 		stat(dirname, &filestat1);
 		//TODO: regex plutôt que la dernière condition
-		if( !(S_ISDIR(filestat1.st_mode) && strcmp(dir1->d_name, ".") && strcmp(dir1->d_name, "..") && strcmp(dir1->d_name, "statemate_modified")) )
+		if( !(S_ISDIR(filestat1.st_mode) && strcmp(dir1->d_name, ".") && strcmp(dir1->d_name, "..") && strcmp(dir1->d_name, "statemate_modified") ))
 			continue;
 		printf("%s\n", dir1->d_name);
 		d2 = opendir(dirname);
@@ -106,42 +141,89 @@ int main()
 			stat(input_filename, &filestat2);
 			if( !S_ISDIR(filestat2.st_mode) ) {
 				char tmp[MAX_LENGTH];
-                                strcpy(tmp, dir2->d_name);
-                                strtok(tmp, "_");
-                                strtok(NULL, "_");
-                                strtok(NULL, "_");
-                                strtok(NULL, "_");
-                                fMEM = atoi(strtok(NULL, "_"));
-				if (fMEM == 200) {
-					avg1 = 0.006950; avg2 = 0.005036; avg3 = 0.004172;
-				}
-				else if (fMEM == 300) {
-					avg1 = 0.005481; avg2 = 0.003733; avg3 = 0.002712;
-				}
-				else if (fMEM == 400) {
-					avg1 = 0.005961; avg2 = 0.003345; avg3 = 0.002721;
-				}
-				else if (fMEM == 500) {
-					avg1 = 0.006023; avg2 = 0.003594; avg3 = 0.002678;
-				}
-				else {
-					fprintf(stderr, "Invalid memory frequency value\n");
-					exit(EXIT_FAILURE);
-				}
+				strcpy(tmp, dir2->d_name);
+				strtok(tmp, "_");
+				strtok(NULL, "_");
+				strtok(NULL, "_");
+				strtok(NULL, "_");
+				fCPU = atoi(strtok(NULL, "_"));
+				strtok(NULL, "_");
+				fMEM = atoi(strtok(NULL, "_"));
 				FILE* input_file = fopen(input_filename, "r");
 				char output_filename[MAX_LENGTH];
 				sprintf(output_filename, "results/energy_measures/%s_modified/modified_%s", dir1->d_name, dir2->d_name);
 				FILE* output_file = fopen(output_filename, "w");
-				if (input_file == NULL || output_file == NULL) {
+				if (input_file == NULL) {
 					// Print an error message to the standard error stream if at least one file cannot be opened.
-					fprintf(stderr, "Unable to open file!\n");
+					fprintf(stderr, "Unable to open input file!\n");
+					return EXIT_FAILURE;
 				}
-				add_FCPU_fMEM_in_csv(input_file, output_file, avg1, avg2, avg3, fMEM);
+				if (output_file == NULL) {
+					// Print an error message to the standard error stream if at least one file cannot be opened.
+					fprintf(stderr, "Unable to open output file!\n");
+					return EXIT_FAILURE;
+				}
+				begin_at_0(input_file, output_file);
+				//add_FCPU_fMEM_in_csv(input_file, output_file, fCPU, fMEM);
 				// Close the file streams.
 				fclose(input_file);
 				fclose(output_file);
 			}
 		}
+		closedir(d2);
 	}
+	closedir(d1);
+
+	d1 = opendir("results/energy_measures");
+    if (!d1) {
+		fprintf(stderr, "Unable to open directory results/energy_measures\n");
+        return EXIT_FAILURE;
+	}
+	while ((dir1 = readdir(d1)) != NULL) {
+		char dirname[MAX_LENGTH];
+		sprintf(dirname, "results/energy_measures/%s", dir1->d_name);
+		stat(dirname, &filestat1);
+		//TODO: regex plutôt que la dernière condition
+		if( !(S_ISDIR(filestat1.st_mode) && strcmp(dir1->d_name, ".") && strcmp(dir1->d_name, "..") && strcmp(dir1->d_name, "statemate")) )
+			continue;
+		printf("%s\n", dir1->d_name);
+		d2 = opendir(dirname);
+		if (!d2) {
+			fprintf(stderr, "Unable to open directory %s\n", dirname);
+        	return EXIT_FAILURE;
+		}
+		while ((dir2 = readdir(d2)) != NULL) {
+			char input_filename[MAX_LENGTH];
+			sprintf(input_filename, "results/energy_measures/%s/%s", dir1->d_name, dir2->d_name);
+			stat(input_filename, &filestat2);
+			if( !S_ISDIR(filestat2.st_mode) ) {
+				char tmp[MAX_LENGTH];
+				strcpy(tmp, dir2->d_name);
+				strtok(tmp, "_");
+				strtok(NULL, "_");
+				strtok(NULL, "_");
+				strtok(NULL, "_");
+				strtok(NULL, "_");
+				fCPU = atoi(strtok(NULL, "_"));
+				strtok(NULL, "_");
+				fMEM = atoi(strtok(NULL, "_"));
+				FILE* input_file = fopen(input_filename, "r");
+				char output_filename[MAX_LENGTH];
+				sprintf(output_filename, "results/energy_measures/%s/modif_%s", dir1->d_name, dir2->d_name);
+				FILE* output_file = fopen(output_filename, "w");
+				if (input_file == NULL || output_file == NULL) {
+					// Print an error message to the standard error stream if at least one file cannot be opened.
+					fprintf(stderr, "Unable to open file!\n");
+					return EXIT_FAILURE;
+				}
+				add_FCPU_fMEM_in_csv(input_file, output_file, fCPU, fMEM);
+				// Close the file streams.
+				fclose(input_file);
+				fclose(output_file);
+			}
+		}
+		closedir(d2);
+	}
+	closedir(d1);
 	return 0;
 }
