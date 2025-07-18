ccf3b56ad298ccdf7d1b1b3c9dde1b1d1a1c6a1a
Myriam Mabrouki <mmabrouk@sta-02008654.paris.inria.fr>
2025-07-18 16:56:02 +0200

    add function to retrieve CPU and memory frequencies in a filname

--- csv_modifier.c
+++ csv_modifier.c
@@ -75,72 +75,48 @@ int add_FCPU_fMEM_in_csv(FILE *input_file, FILE *output_file, int fCPU, int fMEM
 	return 0;
 }
 
-int main()
+int get_CPU_freq_and_MEM_freq(char *str, int *CPU_freq, int *MEM_freq) 
 {
-	DIR *d1, *d2;
-    struct dirent *dir1, *dir2;
-    struct stat filestat1, filestat2;
-	//float avg1, avg2, avg3;
-	int fCPU, fMEM;
+	long int val;
+	int cpt = 0;
+	char *endptr;
+	char * strToken = strtok (str, "_");
+    while ( strToken != NULL) {
+		errno = 0;
+		val = strtol(strToken, &endptr, 10);
 
-	d1 = opendir("results/energy_measures");
-    if (!d1) {
-		fprintf(stderr, "Unable to open directory results/energy_measures\n");
-        return EXIT_FAILURE;
-	}
-	while ((dir1 = readdir(d1)) != NULL) {
-		char dirname[MAX_LENGTH];
-		sprintf(dirname, "results/energy_measures/%s", dir1->d_name);
-		stat(dirname, &filestat1);
-		//TODO: regex plutôt que la dernière condition
-		if( !(S_ISDIR(filestat1.st_mode) && strcmp(dir1->d_name, ".") && strcmp(dir1->d_name, "..") && strcmp(dir1->d_name, "statemate_modified") ))
-			continue;
-		printf("%s\n", dir1->d_name);
-		d2 = opendir(dirname);
-		if (!d2) {
-			fprintf(stderr, "Unable to open directory %s\n", dirname);
+		// Handle errors
+		if (errno == ERANGE || errno == EINVAL) {
+			fprintf(stderr, "strtol error: Invalid value\n");
         	return EXIT_FAILURE;
 		}
-		while ((dir2 = readdir(d2)) != NULL) {
-			char input_filename[MAX_LENGTH];
-			sprintf(input_filename, "results/energy_measures/%s/%s", dir1->d_name, dir2->d_name);
-			stat(input_filename, &filestat2);
-			if( !S_ISDIR(filestat2.st_mode) ) {
-				char tmp[MAX_LENGTH];
-				strcpy(tmp, dir2->d_name);
-				strtok(tmp, "_");
-				strtok(NULL, "_");
-				strtok(NULL, "_");
-				strtok(NULL, "_");
-				fCPU = atoi(strtok(NULL, "_"));
-				strtok(NULL, "_");
-				fMEM = atoi(strtok(NULL, "_"));
-				FILE* input_file = fopen(input_filename, "r");
-				char output_filename[MAX_LENGTH];
-				sprintf(output_filename, "results/energy_measures/%s_modified/modified_%s", dir1->d_name, dir2->d_name);
-				FILE* output_file = fopen(output_filename, "w");
-				if (input_file == NULL) {
-					// Print an error message to the standard error stream if at least one file cannot be opened.
-					fprintf(stderr, "Unable to open input file!\n");
-					return EXIT_FAILURE;
-				}
-				if (output_file == NULL) {
-					// Print an error message to the standard error stream if at least one file cannot be opened.
-					fprintf(stderr, "Unable to open output file!\n");
-					return EXIT_FAILURE;
-				}
-				begin_at_0(input_file, output_file);
-				//add_FCPU_fMEM_in_csv(input_file, output_file, fCPU, fMEM);
-				// Close the file streams.
-				fclose(input_file);
-				fclose(output_file);
-			}
+		if (errno == ERANGE) {
+			fprintf(stderr, "strtol error: Invalid range (too long or too short)\n");
+        	return EXIT_FAILURE;
 		}
-		closedir(d2);
-	}
-	closedir(d1);
 
-	d1 = opendir("results/energy_measures");
+		// If endptr is not at the beginning of the string, an integer value has been converted
+		if (endptr != strToken) {
+			// By convention, we first value should corresponds the 
+			if (!cpt) {
+				*CPU_freq = (int) val;
+				cpt++;
+			}
+			else if (cpt == 1) {
+				*MEM_freq = (int) val;
+				cpt++;
+			}
+			else {
+				fprintf(stderr, "Too much integer values.\n");
+        		return EXIT_FAILURE;
+			}
+			
+		}
+		strToken = strtok (NULL, "_");
+    }
+	return 0;
+}
+
     if (!d1) {
 		fprintf(stderr, "Unable to open directory results/energy_measures\n");
         return EXIT_FAILURE;
