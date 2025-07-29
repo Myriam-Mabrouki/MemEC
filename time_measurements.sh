01b7919d5a8aba72f32d48d626c594fef077cb67
Myriam Mabrouki <mmabrouk@sta-02008654.paris.inria.fr>
2025-07-29 15:36:39 +0200

    update comment for measure in time_measurement

--- time_measurements.sh
+++ time_measurements.sh
@@ -19,7 +19,7 @@ N=$1				#1st argument = number of executions
 fCPU=$2				#2nd argument = current CPU frequency
 fMEM=$3				#3rd argument = current memory frequency
 MEASURE_NAME=execution_time		#name of the measure (for instance: exec time, number of instructions)
-MEASURE=elapsed		#measure(s) (for instance: exec time, number of instructions)
+MEASURE=elapsed					#keyword to get the corresponding measure(s) with the perf command
 #MEASURE=-e task-clock  -e cycles -e instructions -e elapsed
 
 # 2 - Execution N times
