51bfc06faf32e8b8f7aa823407793afb9462f818
Myriam Mabrouki <mmabrouk@sta-02008654.paris.inria.fr>
2025-07-29 15:37:11 +0200

    perf cmd update with measure variables

--- time_measurements.sh
+++ time_measurements.sh
@@ -33,6 +33,6 @@ do
 		mkdir -p results/time_measures/${PRGM/*\/}
 		# Program executed in isolation in core 3
 		# Total CPU time, number of cycles, number of instructions and total elapsed time retrieved
-		taskset -c 3 perf stat $PRGM  2>&1 | grep $MEASURE | awk {'print $1'} >> "results/time_measures/${PRGM/*\/}/${PRGM/*\/}_time_measure_fCPU_${fCPU}_fMEM_${fMEM}.txt"
+		taskset -c 3 perf stat $PRGM  2>&1 | grep $MEASURE | awk {'print $1'} >> "results/time_measures/${PRGM/*\/}/${PRGM/*\/}_${MEASURE_NAME}_fCPU_${fCPU}_fMEM_${fMEM}.txt"
 	done
 done
