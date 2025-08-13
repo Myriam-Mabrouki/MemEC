f8a17155aa4a0d36ad0c21187c76ae0f98e53a02
Myriam Mabrouki <116686541+Myriam-Mabrouki@users.noreply.github.com>
2025-08-13 11:00:55 +0200

    fixed preempt_rt.sh

--- preempt_rt.sh
+++ preempt_rt.sh
@@ -7,4 +7,4 @@ wget https://www.kernel.org/pub/linux/kernel/v6.x/linux-6.9.9.tar.xz
 wget https://www.kernel.org/pub/linux/kernel/projects/rt/6.9/patch-6.9-rt5.patch.xz
 xz -cd linux-6.9.9.tar.xz | tar xvf -
 cd linux-6.9.9
-xzcat ../patch-6.9.9-rt5.patch.xz | patch -p1
+xzcat ../patch-6.9-rt5.patch.xz | patch -p1
