c1cc0850b3aa05ee921dac7a575080cd3a7f39b6
Myriam Mabrouki <myriam.mabrouki@tutanota.com>
2025-08-13 12:05:49 +0200

    preempt-rt has required tools

--- preempt_rt.sh
+++ preempt_rt.sh
@@ -8,6 +8,7 @@ LINUX_VERSION=linux-6.9.9
 PATCH_VERSION=patch-6.9-rt5
 
 cd
+sudo apt install build-essential bc kmod cpio flex cpio libncurses5-dev bison libssl-dev wget
 wget https://www.kernel.org/pub/linux/kernel/v6.x/linux-6.9.9.tar.xz
 wget https://www.kernel.org/pub/linux/kernel/projects/rt/6.9/patch-6.9-rt5.patch.xz
 xz -cd linux-6.9.9.tar.xz | tar xvf -
