f14762f64d33862b817dad15167269d19cb14e5b
Myriam Mabrouki <myriam.mabrouki@tutanota.com>
2025-08-13 12:08:51 +0200

    variables used in preempt-rt

--- preempt_rt.sh
+++ preempt_rt.sh
@@ -9,8 +9,8 @@ PATCH_VERSION=patch-6.9-rt5
 
 cd
 sudo apt install build-essential bc kmod cpio flex cpio libncurses5-dev bison libssl-dev wget
-wget https://www.kernel.org/pub/linux/kernel/v6.x/linux-6.9.9.tar.xz
-wget https://www.kernel.org/pub/linux/kernel/projects/rt/6.9/patch-6.9-rt5.patch.xz
-xz -cd linux-6.9.9.tar.xz | tar xvf -
-cd linux-6.9.9
-xzcat ../patch-6.9-rt5.patch.xz | patch -p1
+wget https://www.kernel.org/pub/linux/kernel/${VERSION}/${LINUX_VERSION}.tar.xz
+wget https://www.kernel.org/pub/linux/kernel/projects/rt/${RT_VERSION}/${PATCH_VERSION}.patch.xz
+xz -cd ${LINUX_VERSION}.tar.xz | tar xvf -
+cd ${LINUX_VERSION}
+xzcat ../${PATCH_VERSION}.patch.xz | patch -p1
