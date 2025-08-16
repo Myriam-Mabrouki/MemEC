003c647a2e35f8acf498cef0cda98ef4b793e3a2
Myriam Mabrouki <116686541+Myriam-Mabrouki@users.noreply.github.com>
2025-08-16 21:49:30 +0200

    no need to patch preempt_rt.sh

--- preempt_rt.sh
+++ preempt_rt.sh
@@ -7,13 +7,13 @@
 
 VERSION=v6.x
 RT_VERSION=6.12
-LINUX_VERSION=linux-6.12.39
+LINUX_VERSION=linux-6.12.25
 PATCH_VERSION=patch-6.12.39-rt11
 
 cd
 sudo apt install build-essential bc kmod cpio flex cpio libncurses5-dev bison libssl-dev wget
 wget https://www.kernel.org/pub/linux/kernel/${VERSION}/${LINUX_VERSION}.tar.xz
-wget https://www.kernel.org/pub/linux/kernel/projects/rt/${RT_VERSION}/${PATCH_VERSION}.patch.xz
+#wget https://www.kernel.org/pub/linux/kernel/projects/rt/${RT_VERSION}/${PATCH_VERSION}.patch.xz
 xz -cd ${LINUX_VERSION}.tar.xz | tar xvf -
 cd ${LINUX_VERSION}
-xzcat ../${PATCH_VERSION}.patch.xz | patch -p1
+#xzcat ../${PATCH_VERSION}.patch.xz | patch -p1
